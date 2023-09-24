b4cc26b029a816dd582dc11356ba9b0db73e1132
AjayRathore21 <ajaykumar420.ak79@gmail.com>
2023-09-24 22:05:45 +0530

    phone no registration api and mongodb database created!

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://ajaykumar420ak79:vSiuKI2mVNzzcaK0@admitkardcluster.aiyol0u.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB Atlas");
+});
--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const PORT = 8000;
+const db = require("./config/mongoose");
+
+const userSchema = require("./models/Users");
 
 app.use(cors());
 
-app.get("/:phone", (req, res) => {
-  console.log(req.params.phone, "body at 7");
-  console.log(typeof parseInt(req.params.phone));
-  var val = Math.floor(1000 + Math.random() * 9000);
-  console.log(val, "10");
-  const otp = {
-    otp: val,
-  };
+app.get("/:phone", async (req, res) => {
+  try {
+    const number = parseInt(req.params.phone);
+    const val = Math.floor(1000 + Math.random() * 9000);
 
-  res.send(JSON.stringify(otp));
+    const filter = { phone: number };
+    const update = { phone: number, otp: val };
+
+    let updatedDocument = await userSchema.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+
+    if (!updatedDocument) {
+      const newUser = new userSchema({
+        phone: number,
+        otp: val,
+      });
+      updatedDocument = await newUser.save();
+    }
+
+    res.json(updatedDocument);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
+
+app.post('/opt',(req,res)=>{
+    console.log(req)
+    // const otp = req.body.otp;
+    // console.log(otp)
+    res.json({otp:'nahi h'})
+
+})
 
 app.listen(PORT, function (err) {
   if (err) console.log(err);
