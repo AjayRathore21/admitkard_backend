005b4ddd2967c51aecc3a65fb66d6cc8576d6469
AjayRathore21 <ajaykumar420.ak79@gmail.com>
2023-09-25 00:48:56 +0530

    all set everything working fine!!

--- index.js
+++ index.js
@@ -3,31 +3,40 @@ const cors = require("cors");
 const app = express();
 const PORT = 8000;
 const db = require("./config/mongoose");
-
 const userSchema = require("./models/Users");
 
+// Middleware
 app.use(cors());
+app.use(express.json());
 
+// GET route for generating OTP and registering users
 app.get("/:phone", async (req, res) => {
   try {
+    // Extract phone number from URL parameter
     const number = parseInt(req.params.phone);
-    const val = Math.floor(1000 + Math.random() * 9000);
 
+    // Generate a random OTP
+    const otp = Math.floor(1000 + Math.random() * 9000);
+
+    // Define the filter and update objects for findOneAndUpdate
     const filter = { phone: number };
-    const update = { phone: number, otp: val };
+    const update = { phone: number, otp };
 
+    // Try to find and update the user, or create a new user if not found
     let updatedDocument = await userSchema.findOneAndUpdate(filter, update, {
       new: true,
     });
 
     if (!updatedDocument) {
+      // If the user was not found, create a new user
       const newUser = new userSchema({
         phone: number,
-        otp: val,
+        otp,
       });
       updatedDocument = await newUser.save();
     }
 
+    // Respond with the updated or newly created user
     res.json(updatedDocument);
   } catch (error) {
     console.error("Error:", error);
@@ -35,15 +44,37 @@ app.get("/:phone", async (req, res) => {
   }
 });
 
+// POST route for OTP verification
+app.post("/opt", (req, res) => {
+  try {
+    // Extract the values of the first 4 keys from the request body
+    const otpValues = Object.values(req.body).slice(0, 4);
 
-app.post('/opt',(req,res)=>{
-    console.log(req)
-    // const otp = req.body.otp;
-    // console.log(otp)
-    res.json({otp:'nahi h'})
+    // Concatenate the values to form a single string
+    const combinedValue = parseInt(otpValues.join(""));
 
-})
+    // Find the user by phone number
+    userSchema
+      .findOne({ phone: req.body.number })
+      .then((document) => {
+        // Check if the OTP matches the one stored in the database
+        if (document.otp === combinedValue) {
+          res.json({ verification: "success" });
+        } else {
+          res.json({ verification: "fail" });
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        res.json({ message: "Some internal error occurred 😢😢😢😢!" });
+      });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
+// Start the server
 app.listen(PORT, function (err) {
   if (err) console.log(err);
   console.log("Server listening on PORT", PORT);
--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
+// Define the User schema
 const userSchema = new mongoose.Schema({
-    phone:Number,
-    otp:Number
-})
+  phone: {
+    type: Number,
+    required: true, // Make 'phone' a required field
+    unique: true, // Ensure 'phone' is unique
+  },
+  otp: {
+    type: Number,
+    required: true, // Make 'otp' a required field
+  },
+});
 
-
-
-module.exports = mongoose.model('User',userSchema)
+// Create and export the User model
+module.exports = mongoose.model("User", userSchema);
